e9bd6566afc9396f56ac191e2f9c01304984ec53
Aaron Boman <aaron@aaronboman.com>
2018-07-03 02:34:02 -0500

    Trying to build documentation.

--- setup.py
+++ setup.py
@@ -12,9 +12,10 @@ if sys.version_info <= (3, 0):
 setup(
     name='granite',
     description='Augments the unittest TestCase API by providing additional utilities useful for Python tools.',
-    email='aaron@aaronboman.com',
+    author_email='aaron@aaronboman.com',
     author='Aaron Boman',
     license='MIT',
+    url='https://github.com/frenchtoast747/granite',
     version='0.0.1',
     long_description=__doc__,
     packages=['granite'],
